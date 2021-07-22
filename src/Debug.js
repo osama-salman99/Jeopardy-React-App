a234a7f3bec6d6b553869ed7a9c791be949522d7
osama <osama.salman99@gmail.com>
2021-07-22 18:47:52 +0300

    update

--- src/Debug.js
+++ src/Debug.js
@@ -1,8 +1,8 @@
-// import {w3cwebsocket as WebSocket} from 'websocket'
-// const socket = new WebSocket('wss://localhost:8433/endpoint', 'echo-protocol');
 import SockJS from 'sockjs-client'
+import SockJsClient from 'react-stomp'
 
 let socket = new SockJS('/endpoint');
+let sockClient = new SockJsClient()
 
 function Debug() {
 	return (
